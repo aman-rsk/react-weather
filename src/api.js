0e319de3565e63158ed579fc177f13e9acb6bf48
aman-rsk <115797086+aman-rsk@users.noreply.github.com>
2023-01-16 14:15:54 +0530

    Second commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "0ade66482bmsh377885ad759f111p1bef12jsnfafa82c179e1",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "06c1aa75c007d838549faca09b06810a";
